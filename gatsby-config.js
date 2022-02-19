01fb7a8ffc86865087d8d4df35e59678504712aa
user.email <efg.river@gmail.com>
2022-02-19 13:56:16 +0900

    update gatsby config gtag origin

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,13 @@ module.exports = {
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
           exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Defaults to https://www.googletagmanager.com
-          origin: "www.efgriver.com",
+          origin: "https://www.googletagmanager.com"
         },
       },
     },
