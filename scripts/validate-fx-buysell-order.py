084ca61f1574fdef8d0d310d9d61b213c9f75e00
user.email <efg.river@gmail.com>
2022-03-27 13:24:07 +0900

    update scripts

--- scripts/validate-fx-buysell-order.py
+++ scripts/validate-fx-buysell-order.py
@@ -1,11 +1,17 @@
 import csv
 import os
+import sys
 
 print("Begin.")
 print("\033[0m")
 last_buysell = None
 
-with open(os.path.join(os.path.dirname(__file__), 'list.csv'), 'r') as f:
+csv_file_path = sys.argv[1]
+if os.path.exists(csv_file_path) == False:
+    print('File {} does not exists, skip.'.format(csv_file_path) )
+    exit(1)
+
+with open(csv_file_path, 'r') as f:
     reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
     if reader is not None:
         headers = next(reader, None)
@@ -15,19 +21,18 @@ with open(os.path.join(os.path.dirname(__file__), 'list.csv'), 'r') as f:
 
         for row in reader:
             if len(row) < 5:
-                print('ERROR: row length is {}. must larger than 5'.format(len(row)))
+                print('ERROR: File {} index={} row column length is {}. must larger than 5'.format(csv_file_path, counter, len(row)))
                 print(row)
                 exit(1)
 
             date = row[1]
-            buysell = print(row[4])
+            buysell = row[4]
 
             # update
             buffer.append(row)
             counter += 1
 
-            date = row[1]
-            if row[4] == "新規売" or row[4] == "新規買":
+            if buysell == "新規売" or buysell == "新規買":
                 index = counter - len(buffer) - 1
                 last_settlement = 0
                 first_open_interest_index = 0
@@ -41,19 +46,19 @@ with open(os.path.join(os.path.dirname(__file__), 'list.csv'), 'r') as f:
                         first_open_interest_index = index
                         first_open_interest_row = row
                         break
+                buffer = []
                 # 順番が不正
                 if first_open_interest_index <= last_settlement:
-                    print("\033[93m[NG]Row number {} buysell is invalid(row order is invalid) row={}".format(first_open_interest_index, first_open_interest_row))
+                    print("\033[93m[NG]File {} Row number {} buysell is invalid(row order is invalid) row={}".format(csv_file_path, first_open_interest_index, first_open_interest_row))
                     print("\033[0mDone.")
                     exit(1)
-                buffer = []
 
         # 新規建玉注文が存在しない決済注文が存在する
         if len(buffer) >= 1:
-            print("\033[93m[NG]Row number {} buysell is invalid(no open interest found) row={}".format(counter, row))
+            print("\033[93m[NG]File {} Row number {} buysell is invalid(no open interest found) row={}, buffer={}".format(csv_file_path, counter, row, buffer))
             print("\033[0mDone.")
             exit(1)
 
 print("\033[0m")
-print("\033[92mRow count={}".format(counter))
+print("\033[92mFile {} Row count={}".format(csv_file_path, counter))
 print("\033[0mDone.")
