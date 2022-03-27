ff4b69c18b9fd6ee60105ed8ebcafa096c12103c
user.email <efg.river@gmail.com>
2022-03-27 18:19:05 +0900

    fix scripts

--- scripts/transform-sbi-csv.py
+++ scripts/transform-sbi-csv.py
@@ -3,8 +3,7 @@
 import sys
 
 # https://docs.python.org/3/tutorial/modules.html
-from sbi_csv_util import transform_to_formal_csv
-from sbi_csv_util import get_formal_csv_path
+import sbi_csv_util
 
 if len(sys.argv) != 3:
     # https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124
@@ -15,12 +14,12 @@ try:
     original_csv_file_path = sys.argv[1]
     dest_csv_dir = sys.argv[2]
 
-    dest_file_path = get_formal_csv_path(original_csv_file_path, dest_csv_dir)
+    dest_file_path = sbi_csv_util.get_formal_csv_path(original_csv_file_path, dest_csv_dir)
 
     with open(original_csv_file_path, 'r') as input_stream:
         with open(dest_file_path, 'w', encoding='utf-8') as output_stream:
 
-            row_count = transform_to_formal_csv(input_stream, output_stream)
+            row_count = sbi_csv_util.transform_to_formal_csv(input_stream, output_stream)
 
             print("\033[92mFile {} Row count={}\033[0m".format(original_csv_file_path, row_count))
 
