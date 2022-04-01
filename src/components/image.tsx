e48405ae26476641835f9f95204be2472efece9c
user.email <efg.river@gmail.com>
2022-04-01 21:26:14 +0900

    replace GatsbyImage using next/image

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,36 +1,10 @@
 import React from 'react'
-// TODO: replace using nextjs component or plain html component
-import { useStaticQuery, graphql } from 'gatsby'
-import { GatsbyImage } from 'gatsby-plugin-image'
+import Image from 'next/image';
+import FxImage20210130134649 from '../images/2021-01-30_13.46.49.png'
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
- */
+const FxImage20210130 = () => {
 
-const Image = () => {
-  // TODO: replace using nextjs component or plain html component
-  // warn [gatsby-transformer-sharp] The "fixed" and "fluid" resolvers are now deprecated. Switch to "gatsby-plugin-image" for better performance and a simpler API. See https://gatsby.dev/migrate-images to learn how.
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "2021-01-30_13.46.49.png" }) {
-        childImageSharp {
-          gatsbyImageData(width: 300, placeholder: BLURRED, layout: FULL_WIDTH)
-        }
-      }
-  }`)
-
-  if (!data?.placeholderImage?.childImageSharp?.gatsbyImageData) {
-    return <div>Picture not found</div>
-  }
-  // TODO: replace using nextjs component or plain html component
-  return <GatsbyImage alt="2021-01-30_13.46.49.png" image={data.placeholderImage.childImageSharp.gatsbyImageData}  imgStyle={{'width':'100%'}} />
+  return <Image alt="2021-01-30_13.46.49.png" src={FxImage20210130134649}  style={{'width':'100%'}} />
 }
 
-export default Image
+export default FxImage20210130
