04fcfb97ea7781da55872f972fc474fba3ba8f21
user.email <efg.river@gmail.com>
2022-04-06 20:16:28 +0900

    rename

--- src/components/admin/app.tsx
+++ src/components/admin/app.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
-import Dashboard from "./fx-analysis/dashboard";
-import FxAnalysisList from "./fx-analysis/list";
+import Dashboard from "./dashboard";
+import FxAnalysisList from "./fx-analysis-list";
 
 const dataProvider = jsonServerProvider(process.env.NEXT_PUBLIC_FX_ANALYSIS_BACKEND_API_ENDPOINT);
 
