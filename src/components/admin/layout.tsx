b8551ce79b65d7ce28876190dbe01c0a4543ad2a
藤川英悟 <efg.river@gmail.com>
2022-04-07 11:58:14 +0900

    Update layout.tsx

--- src/components/admin/layout.tsx
+++ src/components/admin/layout.tsx
@@ -3,7 +3,7 @@ import MyAppBar from './appbar';
 import MyMenu from './menu';
 import MyNotification from './notification';
 
-const MyLayout = (props) => <Layout
+const MyLayout = (props:any) => <Layout
     {...props}
     appBar={MyAppBar}
     menu={MyMenu}
