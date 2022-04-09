aa85985fda63a7ae7e3dd4c14b018cdb7a6ac251
user.email <efg.river@gmail.com>
2022-04-09 16:33:42 +0900

    @emotion対応, @muiを理解した

--- src/components/fx-analysis/app.tsx
+++ src/components/fx-analysis/app.tsx
@@ -1,21 +1,29 @@
 import * as React from "react";
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import MyDashboard from "./ra-customize/dashboard";
 import FxAnalysisList from "./ra-resources/fx-analysis-list";
 import MyLayout from './ra-customize/layout';
 
+
 const dataProvider = jsonServerProvider(process.env.NEXT_PUBLIC_FX_ANALYSIS_BACKEND_API_ENDPOINT);
+const theme = createTheme({
+  direction: 'rtl',
+});
 
 const AdminApp = () => (
-  <Admin 
-    layout={MyLayout} 
-    dashboard={MyDashboard} 
-    dataProvider={dataProvider}
-    disableTelemetry>
-    <Resource name="fx-analysis" list={FxAnalysisList} icon={AttachMoneyIcon} />
-  </Admin>
+  <ThemeProvider theme={theme}>
+    <Admin 
+      layout={MyLayout} 
+      dashboard={MyDashboard} 
+      dataProvider={dataProvider}
+      disableTelemetry>
+      <Resource name="fx-analysis" list={FxAnalysisList} icon={AttachMoneyIcon} />
+    </Admin>
+  </ThemeProvider>
 );
 
 export default AdminApp;
